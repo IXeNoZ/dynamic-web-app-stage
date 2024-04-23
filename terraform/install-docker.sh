3cdc11212f4d5184ecd5fd7758349e017ed21b45
IXeNoZ <ixenoz5@gmail.com>
2024-04-23 11:53:32 +0200

    Update install-docker.sh

--- terraform/install-docker.sh
+++ terraform/install-docker.sh
@@ -6,5 +6,5 @@ sudo service docker start
 sudo usermod -a -G docker ec2-user
 sudo docker stop website || true
 sudo docker rm website || true
-sudo docker pull lyndaaaudrey/website-repo:latest
+sudo docker pull loic591/web-app:latest
 sudo docker run -d --name website -p 80:80 loic591/web-app:latest
